36f7bf1ed3b69f5ff74b0adf5ce71f8a1e5622b6
Yusuke Akiyama <akiyamaysk0321@gmail.com>
2022-01-07 22:19:07 +0900

    Add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => {
+  return <footer>© 2022 yusuke akiyama</footer>;
+};
+
+export default Footer;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '~/components/Header';
+import Footer from '~/components/Footer';
 
 const Layout: React.FC = (props) => {
   return (
     <div className="bg-slate-700">
       <Header />
       {props.children}
+      <Footer />
     </div>
   );
 };
